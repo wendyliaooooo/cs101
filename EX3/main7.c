e1852e9143e355ed0c5477b1465df243b7e87988
wendyliaooooo <940930wendy1@gmail.com>
2025-10-02 21:55:48 +0800

    Update main7.c

--- EX3/main7.c
+++ EX3/main7.c
@@ -1,1 +1,18 @@
+int i =1599;
+    if(i <=1500) {
+        printf("70元");
+    }
+    else {    
+        int n = i - 1500;
+        if (n % 100) {
+            int h = ((n/100)+1) * 10;
+            printf("%d元", 70 + h );
+        }
+        else {
+            printf("%d 元", 70 + (n/100) * 10);
+        }
+        
+     }
+    return 0;
+}
 
