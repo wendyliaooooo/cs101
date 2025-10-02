73e8b0c07dff97624a9018d059e3f0d05ae753ba
wendyliaooooo <940930wendy1@gmail.com>
2025-10-02 19:51:00 +0800

    Update main3.c

--- EX3/main3.c
+++ EX3/main3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int i = 9;
+
+    if (i % 15 == 0) {
+        printf("LOVE IU");
+    }
+    else if (i % 3 == 0) {
+        printf("LOVE");
+    }
+    else if (i % 5 == 0) {
+        printf("IU");
+    }
+    else {
+        printf("%d", i);
+    }
+
+    return 0;
+}
