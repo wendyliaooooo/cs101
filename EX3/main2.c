9c26e5779526ef74c8929f25895b9a77d0955b87
wendyliaooooo <940930wendy1@gmail.com>
2025-10-02 18:30:04 +0800

    Update main2.c

--- EX3/main2.c
+++ EX3/main2.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-int i = 10;
-if ((i & (i - 1)) == 0) { 
+int i = 1;
+if (i>1 && (i & (i - 1)) == 0) { 
 printf("ture\n"); }
 else {
 printf("false\n");
 }
 return 0;
 }
-
