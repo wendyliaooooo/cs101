f59ddcada02f993fcf35c905be8f19d35e61a4d1
wendyliaooooo <940930wendy1@gmail.com>
2025-10-02 17:45:41 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+void print_sp(int i, int n) {
+    for (int sp = 0; sp < n-i;sp++) {
+        printf(" ");
+    }
+}
+    void print_num(int n) {
+        for (int i = 1; i<= n; i++) {
+            printf(" %d",n);
+        }
+        printf("\n");
+    }
+
+int main() {
+   int rows = 6;
+for(int i =0;i<=rows; i++) {
+        print_sp(i, rows);
+        print_num(i);
+    
+    }
+    
+ return 0;
+ }
 
